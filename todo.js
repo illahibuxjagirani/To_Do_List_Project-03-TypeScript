1cbcabd9743118b2627a9157391d8fb8ee0e6657
ILLAHIB <155870604+ILLAHIB@users.noreply.github.com>
2024-01-08 09:44:40 +0500

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,34 @@
+import inquirer from "inquirer";
+let todoString = [];
+let loop = true;
+while (loop) {
+    const todoInput = await inquirer.prompt([
+        {
+            type: "input",
+            name: "todoItem",
+            message: "Enter item Name which you want to add in your To-Do list"
+        },
+        {
+            type: "confirm",
+            name: "addMore",
+            message: "Do you want to add more item in your To-Do list?",
+            default: false
+        }
+    ]);
+    const todoItem = todoInput.todoItem;
+    const addMore = todoInput.addMore;
+    if (todoItem) {
+        todoString.push(todoItem);
+    }
+    loop = addMore;
+}
+;
+if (todoString.length > 0) {
+    console.log(`Here is your To-Do list:`);
+    for (let i = 0; i < todoString.length; i++) {
+        console.log(`${i + 1}. ${todoString[i]}`);
+    }
+}
+else {
+    console.log(`Your To-Do list is Empty!`);
+}
